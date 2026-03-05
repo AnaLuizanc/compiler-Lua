e7ad5d259366c895c6871073041976f5cf37cbae
AnaLuizanc <analuizanc13@gmail.com>
2026-03-05 10:08:19 -0300

    refactor: refine Lua file processing with improved variable handling and syntax corrections

--- lexicalAnalysis/scripts/compil-lab1-amostra-B-ana-luiza-nobre-cordeiro.lua
+++ lexicalAnalysis/scripts/compil-lab1-amostra-B-ana-luiza-nobre-cordeiro.lua
@@ -29,26 +29,26 @@ function func(v, n, T)
     if n<=0
         then return 1.0
     elseif n==1
-        then return 1.01 + v[0].x / 1.e2 + v[0].y / 0.1e-2 - T.a.x * T.a.x + T.b.x*T.c.x;
+        then return 1.01 + v[1].x / 1.e2 + v[1].y / 0.1e-2 - T.a.x * T.a.x + T.b.y*T.c.x
     end
-    res = 0.25e-13;
-    i = n-1;
-    while i >= 0 and v[i].x > 0 do
-        temp = v[i].y * v[i].x % 123;
+    local res = 0.25e-13
+    i = n-1
+    while i >= 1 and v[i].x > 0 do
+        temp = v[i].y * v[i].x % 123
         if (temp < 0.0)
-            then res = res - res*2.e-2 +  func(v, n-1, T) * temp - T.a.y*T.cor;
+            then res = res - res*2.e-2 +  func(v, n-1, T) * temp - T.a.y*T.cor
         else
-            res = res + res*0.3e3 +func(v, n-2, T) * temp + T.c.x*T.cor;
-            printf("Estranho, ne?\n");
+            res = res + res*0.3e3 +func(v, n-2, T) * temp + T.c.x*T.cor
+            print("Estranho, ne?\n")
         end
         i = i-1
     end
-    return res;
+    return res
 end
 
 function F2(T)
-    A = 0
-    soma[10]
+    local A = 0
+    soma = {}
     if (T.a.x >= 10 or T.b.y > 20 or T.a.y < 30 or T.b.x <= 50) and (not(T.c.x ~= 90 or T.c.y == 0))
         then return 10 % 3
     else
@@ -56,7 +56,7 @@ function F2(T)
     end
 
     while A < 10 do
-        total = 0
+        local total = 0
         total = total + T.c.x * T.c.y;
         total = total + T.b.x * T.a.y;
         total = total + T.a.x * T.b.y;
