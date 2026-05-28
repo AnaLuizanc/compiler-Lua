f1a29332dc2be84f00b0a8d43fd5a88c50c86382
AnaLuizanc <analuizanc13@gmail.com>
2026-05-28 18:56:20 -0300

    refactor(semantic-analysis): replace function extraction with command extraction and update default grammar file

--- lab3-semanticAnalysis/src/sol.cpp
+++ lab3-semanticAnalysis/src/sol.cpp
@@ -12,8 +12,9 @@ using namespace std;
 #include "TabelaLR1.hpp"
 #include "Arvore.hpp"
 #include "Parser.hpp"
-#include "Funcao.hpp"
-#include "Analisador.hpp"
+#include "Comando.hpp"
+// #include "Funcao.hpp"
+// #include "Analisador.hpp"
 
 
 int main(int argc, char * argv[]) {
@@ -24,7 +25,7 @@ int main(int argc, char * argv[]) {
   string nome_gramatica, nome_tab_lr1;
   if (argc == 1) {
     //cerr << "Valores padrao utilizados: gramatica9.site e tabela_lr1.conf" << endl;
-    nome_gramatica = string("gramatica9.site");
+    nome_gramatica = string("gramatica-0.site");
     nome_tab_lr1 = string("tabela_lr1.conf");
   } else {
     nome_gramatica = string(argv[1]);
@@ -47,18 +48,18 @@ int main(int argc, char * argv[]) {
   Arvore_parse arv = parser.executa_parse(cin);
   cerr << "Parse executado" << endl;
   arv.debug();
-  Funcao* func = Funcao::extrai_funcao(arv.raiz);
-  func->debug();
+  Comando* cmd = Comando::extrai_comando(arv.raiz);
+  cmd->debug();
   // Exemplo de chamada do analisador semantico.
-  vector<ValorLiteral> parametros_passados;
-  for (int i = 1; i <= 3; ++i) {
-    ValorLiteral valor_parametro;
-    valor_parametro.tipo = new Tipo(Tipo::INT);
-    valor_parametro.valor_int = i*10;
-    parametros_passados.push_back(valor_parametro);
-  }
-  Analisador ana;
-  cout << "Retorno calculado:" << endl;
-  cout << ana.calcula_retorno(func, parametros_passados) << endl;
+//   vector<ValorLiteral> parametros_passados;
+//   for (int i = 1; i <= 3; ++i) {
+//     ValorLiteral valor_parametro;
+//     valor_parametro.tipo = new Tipo(Tipo::INT);
+//     valor_parametro.valor_int = i*10;
+//     parametros_passados.push_back(valor_parametro);
+//   }
+//   Analisador ana;
+//   cout << "Retorno calculado:" << endl;
+//   cout << ana.calcula_retorno(func, parametros_passados) << endl;
   return 0;
 }
