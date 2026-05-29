6f9022b118ccd91ff67130ac0646bbff423dd922
AnaLuizanc <analuizanc13@gmail.com>
2026-05-29 11:54:37 -0300

    refactor(semantic-analysis): remove unused `Comando` class and associated methods, update Makefile and grammar files to use default grammar

--- lab3-semanticAnalysis/src/sol.cpp
+++ lab3-semanticAnalysis/src/sol.cpp
@@ -12,8 +12,7 @@ using namespace std;
 #include "TabelaLR1.hpp"
 #include "Arvore.hpp"
 #include "Parser.hpp"
-#include "Comando.hpp"
-// #include "Funcao.hpp"
+#include "Funcao.hpp"
 // #include "Analisador.hpp"
 
 
@@ -25,7 +24,7 @@ int main(int argc, char * argv[]) {
   string nome_gramatica, nome_tab_lr1;
   if (argc == 1) {
     //cerr << "Valores padrao utilizados: gramatica9.site e tabela_lr1.conf" << endl;
-    nome_gramatica = string("gramatica-0.site");
+    nome_gramatica = string("gramatica-1.site");
     nome_tab_lr1 = string("tabela_lr1.conf");
   } else {
     nome_gramatica = string(argv[1]);
@@ -35,31 +34,38 @@ int main(int argc, char * argv[]) {
   ifstream arq_gramatica(nome_gramatica);
   ifstream arq_tabela_lr1(nome_tab_lr1);
   if (arq_tabela_lr1.fail() || arq_gramatica.fail()) {
-    cerr << "Falha ao abrir arquivos: " << 
-      ((arq_gramatica.fail()) ? nome_gramatica : "") << ", " << 
+    cerr << "Falha ao abrir arquivos: " <<
+      ((arq_gramatica.fail()) ? nome_gramatica : "") << ", " <<
       ((arq_tabela_lr1.fail()) ? nome_tab_lr1 : "") << endl;
-    return 1;    
+    return 1;
   }
   Parser parser(arq_gramatica, arq_tabela_lr1);
   parser.tabela.debug();
   parser.gram.debug();
 
-  
+
   Arvore_parse arv = parser.executa_parse(cin);
   cerr << "Parse executado" << endl;
   arv.debug();
-  Comando* cmd = Comando::extrai_comando(arv.raiz);
-  cmd->debug();
+
+  if(arv.raiz == nullptr) {
+    cerr << "Erro: arvore de parse vazia" << endl;
+    return 1;
+  }
+  // Funcao* func = Funcao::extrai_funcao(arv.raiz);
+  // func->debug();
+  ID* id = ID::extrai_ID(arv.raiz->filhos[0]->filhos[1]);
+  cerr << "ID extraido: " << id->nome << endl;
   // Exemplo de chamada do analisador semantico.
-//   vector<ValorLiteral> parametros_passados;
-//   for (int i = 1; i <= 3; ++i) {
-//     ValorLiteral valor_parametro;
-//     valor_parametro.tipo = new Tipo(Tipo::INT);
-//     valor_parametro.valor_int = i*10;
-//     parametros_passados.push_back(valor_parametro);
-//   }
-//   Analisador ana;
-//   cout << "Retorno calculado:" << endl;
-//   cout << ana.calcula_retorno(func, parametros_passados) << endl;
+  // vector<ValorLiteral> parametros_passados;
+  // for (int i = 1; i <= 3; ++i) {
+  //   ValorLiteral valor_parametro;
+  //   valor_parametro.tipo = new Tipo(Tipo::INT);
+  //   valor_parametro.valor_int = i*10;
+  //   parametros_passados.push_back(valor_parametro);
+  // }
+  // Analisador ana;
+  // cout << "Retorno calculado:" << endl;
+  // cout << ana.calcula_retorno(func, parametros_passados) << endl;
   return 0;
 }
