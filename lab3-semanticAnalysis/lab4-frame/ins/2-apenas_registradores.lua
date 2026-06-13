af9eb8e43d2ae68173594ea29e00af526df16a81
AnaLuizanc <analuizanc13@gmail.com>
2026-06-13 13:10:26 -0300

    feat(samples): add lua examples for lab4

---
+++ lab3-semanticAnalysis/lab4-frame/ins/2-apenas_registradores.lua
@@ -0,0 +1,9 @@
+function matematica(a:int):int
+    local x:int
+    local y:int
+    local z:int
+    x = 10
+    y = 20
+    z = x + y * a
+    return z
+end
---
+++ lab3-semanticAnalysis/lab4-frame/ins/3-escape_basico.lua
@@ -0,0 +1,9 @@
+function calculaArea(base:int, altura:int):int
+    local area:int
+    local multiplicador:int
+    multiplicador = 1
+    base = 5
+    altura = 10
+    area = externa(base, altura) * multiplicador
+    return area
+end
